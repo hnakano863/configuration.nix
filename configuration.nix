a32c3ad6f37190a361595064920ae11023ae3e73
hnakano863 <notchi863@gmail.com>
2021-03-06 17:26:57 +0900

    enable emacs daemon

--- configuration.nix
+++ configuration.nix
@@ -160,6 +160,9 @@
     DBs = with pkgs.dictdDBs; [ wiktionary wordnet eng2jpn ];
   };
 
+  services.emacs.enable = true;
+  services.emacs.package = config.home-manager.users.hnakano.programs.emacs.finalPackage;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
