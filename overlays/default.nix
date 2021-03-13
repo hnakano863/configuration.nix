cd5c5afb8334be69864399b198fa7882a0d4fa72
hnakano863 <notchi863@gmail.com>
2021-03-13 16:14:20 +0900

    Install Julia v1.5.4

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,17 +11,6 @@ final: prev: {
     jsoncpp = final.jsoncpp;
   };
 
-  # julia
-  julia-1_5_2 = final.callPackage ./julia/template.nix {
-    version = "1.5.2";
-    sha256 = "0k3bpw8v07b70z50dyvjgz5wc58bnvw505lfcssczmhpfqd5zj49";
-  };
-
-  julia-bin = final.callPackage ./julia/template.nix {
-    version = "1.5.3";
-    sha256 = "1yc60dl39sa0rbiaj2v7vf79j0c9zd93h1mwcahq99w44c81k3q6";
-  };
-
   # jupyter-command
   jupyterCmdFHS = import ./jupyterCmdFHS final prev;
 
@@ -42,4 +31,4 @@ final: prev: {
   dictdDBs = prev.dictdDBs // {
     eng2jpn = final.callPackage ./dictd-db/eng-jpn.nix {};
   };
-}
+} // (import ./julia final prev)
--- overlays/julia/default.nix
+++ overlays/julia/default.nix
@@ -1,3 +1,17 @@
 final: prev: {
+  julia-1_5_2 = final.callPackage ./template.nix {
+    version = "1.5.2";
+    sha256 = "0k3bpw8v07b70z50dyvjgz5wc58bnvw505lfcssczmhpfqd5zj49";
+  };
+
+  julia-1_5_3 = final.callPackage ./template.nix {
+    version = "1.5.3";
+    sha256 = "1yc60dl39sa0rbiaj2v7vf79j0c9zd93h1mwcahq99w44c81k3q6";
+  };
+
+  julia-bin = final.callPackage ./template.nix {
+    version = "1.5.4";
+    sha256 = "0bb8fr692dqrf718p644y5swd3z8ivh4ysdaw7q0cir10cc16nym";
+  };
 
 }
