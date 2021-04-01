901116000df747afb2be91c8ec7661444d93df99
hnakano863 <notchi863@gmail.com>
2021-04-01 20:58:45 +0900

    update julia

--- overlays/julia/template.nix
+++ overlays/julia/template.nix
@@ -1,15 +1,20 @@
 { stdenv
 , autoPatchelfHook
-, version
+, major
+, minor
+, patch
 , sha256
 }:
-
+let
+  version = "${major}.${minor}.${patch}";
+  key = "${major}.${minor}";
+in
 stdenv.mkDerivation rec {
   pname = "julia-bin";
   inherit version;
 
   src = builtins.fetchTarball {
-    url = "https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-${version}-linux-x86_64.tar.gz";
+    url = "https://julialang-s3.julialang.org/bin/linux/x64/${key}/julia-${version}-linux-x86_64.tar.gz";
     inherit sha256;
   };
 
