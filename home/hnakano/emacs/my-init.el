941ee7e81a222ebedf53e1f57c5f28f935a0de52
hnakano863 <notchi863@gmail.com>
2021-06-02 22:25:20 +0900

    多分lsp-javascriptの後に設定する必要がある

--- home/hnakano/emacs/my-init.el
+++ home/hnakano/emacs/my-init.el
@@ -247,10 +247,13 @@
       (lsp-keymap-prefix . "C-c C-l")
       (lsp-auto-configure . t)
       (lsp-nix-server-path . "@rnixlsp@/bin/rnix-lsp")
-      (lsp-clients-typescript-tls-path . "@tsls@/bin/typescript-language-server")
       (lsp-enable-folding . t)
       :config
-      (lsp-dependency 'typescript '(:system "@typescript@/bin/tsserver")))
+      (leaf lsp-javascript
+	:custom
+	(lsp-clients-typescript-tls-path . "@tsls@/bin/typescript-language-server")
+	:config
+	(lsp-dependency 'typescript '(:system "@typescript@/bin/tsserver"))))
     (leaf lsp-ui
       :custom (lsp-ui-sideline-show-hover . t))))
 
