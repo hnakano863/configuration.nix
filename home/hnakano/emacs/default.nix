85568c97883cc7fa0bd10c0badeb8d476ff3c4d5
hnakano863 <notchi863@gmail.com>
2021-07-10 01:19:25 +0900

    emacsGccは不便だったので戻す

--- home/hnakano/emacs/default.nix
+++ home/hnakano/emacs/default.nix
@@ -19,7 +19,7 @@ in
 
   programs.emacs.enable = true;
   programs.emacs.overrides = import ./overrides { inherit pkgs; };
-  programs.emacs.package = emacsGcc;
+  programs.emacs.package = emacs;
 
   programs.emacs.compileInit = {
     enable = true;
