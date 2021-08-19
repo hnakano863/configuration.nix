a27db4649f6069761b25d798a3d4e21b318df96f
hnakano863 <notchi863@gmail.com>
2021-08-19 19:38:40 +0900

    install dropbox

--- home/hnakano/home.nix
+++ home/hnakano/home.nix
@@ -129,6 +129,8 @@
     terminal = "${pkgs.alacritty}/bin/alacritty";
   };
 
+  services.dropbox.enable = true;
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
