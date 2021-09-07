aeaf658a71dab461c12f278d6679519dc57380a8
Hiroshi Nakano <hnakano@tlv.co.jp>
2021-09-08 08:59:56 +0900

    wsl上でのXのハック

--- home/hnakano/wsl2.nix
+++ home/hnakano/wsl2.nix
@@ -6,4 +6,8 @@
   programs.git.userName = "hnakano";
   programs.git.userEmail = "hnakano@tlv.co.jp";
 
+  programs.bash.profileExtra = ''
+    export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+  '';
+
 }
