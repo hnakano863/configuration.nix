c29fe995149bfc8f572564b0727799562ec3c502
hnakano863 <notchi863@gmail.com>
2021-09-18 00:26:22 +0900

    install and configured trayer

--- home/hnakano/linux.nix
+++ home/hnakano/linux.nix
@@ -65,4 +65,17 @@
 
   services.dropbox.enable = true;
 
+  services.trayer.enable = true;
+  services.trayer.settings = {
+    edge = "top";
+    align = "right";
+    SetDockType = true;
+    SetPartialStrut = true;
+    expand = true;
+    width = 10;
+    transparent = true;
+    tint = "0x5f5f5f";
+    height = 18;
+  };
+
 }
