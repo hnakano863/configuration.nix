6d87c6d70676441ac58d5355fe568b65aa9ab26d
hnakano863 <notchi863@gmail.com>
2021-11-20 18:28:04 +0900

    試みに、ログインシェルをfishにする

--- users.nix
+++ users.nix
@@ -11,6 +11,7 @@
         isNormalUser = true;
         createHome = true;
         hashedPassword = "$6$P6rnLO2gX.Q9r0B/$Sw3TJAdNBn9E2XWmOJUndXEM.8WxkN8eEGZ3VOMQ3q.IPmfBbzyTc1bctbctbtJUG5TXDq7wH/wTbWk68Fcaf/";
+        shell = pkgs.fish;
         extraGroups = [
           "wheel"
           "networkmanager"
