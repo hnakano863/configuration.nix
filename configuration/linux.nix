69865b0f98a186f321e5c1e64300adb3354e10b8
hnakano863 <notchi863@gmail.com>
2022-04-06 20:14:27 +0900

    Enabled OpenGL

--- configuration/linux.nix
+++ configuration/linux.nix
@@ -108,6 +108,9 @@
     };
   };
 
+  # Enable opengl
+  hardware.opengl.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
