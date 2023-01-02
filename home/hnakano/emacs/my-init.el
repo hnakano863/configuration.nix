9099b109bc4009e5322b2f80f0fca3937a340dcb
hnakano863 <notchi863@gmail.com>
2023-01-02 14:08:50 +0900

    set emacs font

--- home/hnakano/emacs/my-init.el
+++ home/hnakano/emacs/my-init.el
@@ -193,9 +193,8 @@
       '(bar window-number modals buffer-info " " buffer-position)
       '(misc-info process checker repl lsp vcs indent-info buffer-encoding "   "))
     (doom-modeline-set-modeline 'my/main t)
-    ; (set-face-attribute 'mode-line nil :family "Cica" :height 120)
-    ; (set-face-attribute 'mode-line-inactive nil :family "Cica" :height 120)
-    ))
+    (set-face-attribute 'mode-line nil :family "HackGen" :height 120)
+    (set-face-attribute 'mode-line-inactive nil :family "HackGen" :height 120)))
 
 (leaf shackle
   :global-minor-mode t
