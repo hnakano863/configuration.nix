bab7070b76561659a2faa503fea4b612a1dcbdd4
hnakano <hnakano@tlv.co.jp>
2023-05-24 12:48:13 +0900

    set prism color theme

--- home/hnakano/emacs/my-init.el
+++ home/hnakano/emacs/my-init.el
@@ -235,7 +235,11 @@
       :config
       (sp-local-pair 'org-mode "\\[" "\\]")))
   (leaf rainbow-delimiters
-    :hook ((emacs-lisp-mode-hook org-mode-hook) . rainbow-delimiters-mode)))
+    :hook ((emacs-lisp-mode-hook org-mode-hook) . rainbow-delimiters-mode))
+  (leaf prism
+    :config
+    (prism-set-colors :lightens '(0 5 10) :desaturations '(-2.5 0 2.5)
+      :colors (-map #'doom-color '(red orange yellow green blue violet)))))
 
 (leaf language
   :doc "programming language setup"
