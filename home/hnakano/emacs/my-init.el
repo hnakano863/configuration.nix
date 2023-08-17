cbc7e38f6ebd03ab3a74345c37f31de4e8387642
hnakano863 <notchi863@gmail.com>
2023-08-17 18:36:13 +0900

    fix for https://github.com/bbatsov/projectile/issues/1777

--- home/hnakano/emacs/my-init.el
+++ home/hnakano/emacs/my-init.el
@@ -121,7 +121,24 @@
   (projectile-indexing-method . 'alien)
   (projectile-sort-order . 'recentf-active)
   (projectile-enable-caching . t)
-  (projectile-project-search-path . '("~/repos/" "~/experiments/" "~/ghq/")))
+  (projectile-project-search-path . '("~/repos/" "~/experiments/" "~/ghq/"))
+  ; fix for https://github.com/bbatsov/projectile/issues/1777
+  (projectile-globally-ignored-directories . '(".idea"
+					       ".vscode"
+					       ".ensime_cache"
+					       ".eunit"
+					       ".git"
+					       ".hg"
+					       ".fslckout"
+					       "_FOSSIL_"
+					       ".bzr"
+					       "_darcs"
+					       ".tox"
+					       ".svn"
+					       ".stack-work"
+					       ".ccls-cache"
+					       ".cache"
+					       ".clangd")))
 
 (leaf treemacs
   :doc "treemacs and its extentions"
