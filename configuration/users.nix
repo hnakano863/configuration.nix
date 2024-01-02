c3d18f5806f954b9a7a638bbea01ee0695c9f195
hnakano863 <notchi863@gmail.com>
2024-01-02 19:08:25 +0900

    don't use fish as login shell

--- configuration/users.nix
+++ configuration/users.nix
@@ -11,7 +11,6 @@
         isNormalUser = true;
         createHome = true;
         hashedPassword = "$6$P6rnLO2gX.Q9r0B/$Sw3TJAdNBn9E2XWmOJUndXEM.8WxkN8eEGZ3VOMQ3q.IPmfBbzyTc1bctbctbtJUG5TXDq7wH/wTbWk68Fcaf/";
-        shell = pkgs.fish;
         extraGroups = [
           "wheel"
           "networkmanager"
