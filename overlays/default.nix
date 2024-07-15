c4bbcb8b3665c19a76e5aa5aaa6cdafacd457789
hnakano863 <notchi863@gmail.com>
2024-07-15 20:42:53 +0900

    add skk-server related overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,4 +2,10 @@ final: prev: {
 
   jupyterCommand = import ./jupyter-command { pkgs = prev; };
 
+  # for skk server
+  skk-dicts = final.callPackage ./skk-dicts/override.nix { inherit (prev) skk-dicts; };
+  skk-dicts-cdb = final.callPackage ./skk-dicts/skk-dicts-cdb.nix {};
+  yaskkserv2 = final.callPackage ./skk-dicts/yaskkserv2.nix {};
+  dbskkd-cdb = final.callPackage ./skk-dicts/dbskkd-cdb.nix {};
+
 }
