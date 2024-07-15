66bfd312ecd0c9110591d22a8b69a69dd5a074f8
hnakano863 <notchi863@gmail.com>
2024-07-15 23:31:09 +0900

    set default browser

--- home/hnakano/linux.nix
+++ home/hnakano/linux.nix
@@ -59,4 +59,15 @@
 
   services.dropbox.enable = true;
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "vivaldi-stable.desktop";
+      "x-scheme-handler/http" = "vivaldi-stable.desktop";
+      "x-scheme-handler/https" = "vivaldi-stable.desktop";
+      "x-scheme-handler/about" = "vivaldi-stable.desktop";
+      "x-scheme-handler/unknown" = "vivaldi-stable.desktop";
+    };
+  };
+
 }
