fee22c50c5124e69319c67147a39417d5a04aaf3
hnakano863 <notchi863@gmail.com>
2024-12-03 23:49:53 +0900

    fix script

--- overlays/skk-dicts/cdb.nix
+++ overlays/skk-dicts/cdb.nix
@@ -9,10 +9,10 @@ let
   mkCdbFile = name: value: runCommand "skk-jisyo-cdb-${name}-unstable" {
     buildInputs = [ python3 ];
   } ''
-    for s in $(ls ${value}/share/skk | grep SKK-JISYO | basename); do
+    for s in $(ls ${value}/share/skk | grep SKK-JISYO); do
       python ${skktools.src}/skk2cdb.py -f cdb.tmp \
-        ${value}/share/skk/$s
-      install -Dm 644 cdb.tmp $out/share/skk/$s.cdb
+        ${value}/share/skk/$(basename s)
+      install -Dm 644 cdb.tmp $out/share/skk/$(basename s).cdb
     done
   '';
 
