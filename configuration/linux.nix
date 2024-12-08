56c6dc68bbf16318d1ae19657a8667012de26377
hnakano863 <notchi863@gmail.com>
2024-12-08 22:06:22 +0900

    enable input method

--- configuration/linux.nix
+++ configuration/linux.nix
@@ -38,6 +38,7 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "ja_JP.UTF-8";
   i18n.inputMethod = {
+    enable = true;
     type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-gtk
