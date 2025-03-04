14aa834bc4effebcb3b0d11ef956db307c2223f4
hnakano863 <notchi863@gmail.com>
2025-03-04 14:28:33 +0900

    feat(deps): update @dataform/cli to 3.0.15

--- overlays/dataform-cli/src.nix
+++ overlays/dataform-cli/src.nix
@@ -1,5 +1,5 @@
 { fetchurl, version }:
 fetchurl {
   url = "https://registry.npmjs.org/@dataform/cli/-/cli-${version}.tgz";
-  sha512 = "RGOi3SWFAZM1HqAG9rK73ydoSSMqJy3+BaX/LE5eficT5dd3QQ+3+Ojd+6dw0wN3bZNCw5VYdYLVuQFmV5OMhA==";
+  sha512 = "H4zM6bvi9Xjo8ODjO0HJYFqAMXvgDrvoJFxMoGbgKFg2rX+/K3WJJTHmDZmKI4ksv9tvcYw8zdoWuYa7TC68kQ==";
 }
