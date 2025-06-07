347627c146bb8886db769c7bd00dc4a0ed6e8ea8
hnakano863 <notchi863@gmail.com>
2025-06-07 19:31:36 +0900

    update julia registry

--- home/hnakano/language/julia/default.nix
+++ home/hnakano/language/julia/default.nix
@@ -9,13 +9,20 @@ let
     inherit jupyterCommand;
   } ''substituteAll "${./startup.jl.in}" $out '';
 
-  myJulia = julia.withPackages [
+  myJulia = (julia.withPackages.override {
+    augmentedRegistry = fetchFromGitHub {
+      owner = "CodeDownIO";
+      repo = "General";
+      rev = "8931e0b2c4359c8764c9911ef82ca1e21f9e21e1";
+      hash = "sha256-rTkDLAiiQj2LDyndi+aPNCPvZBeuAsjsU8tGKh1A8/8=";
+    };
+  }) [
     "BenchmarkTools"
     "DrWatson"
     "IJulia"
     "LanguageServer"
     "OhMyREPL"
-    # "Pluto"
+    "Pluto"
     "Revise"
     "JLD2" # required for DrWatson
   ];
