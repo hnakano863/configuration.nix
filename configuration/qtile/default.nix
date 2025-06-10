bae4aa099f1410bba5f7331ef3eccc84f9ea8c6d
hnakano863 <notchi863@gmail.com>
2025-06-10 19:49:31 +0900

    Don't use qtile.configFile for error

--- configuration/qtile/default.nix
+++ configuration/qtile/default.nix
@@ -3,11 +3,16 @@
 {
   services.xserver.windowManager.qtile = {
     enable = true;
-    configFile = ./config.py;
   };
 
   # Additional packages for qtile
   environment.systemPackages = with pkgs; [
     dex
   ];
+
+  # config.py
+  home-manager.users.hnakano = { config, pkgs, lib, ... }: {
+    home.file.".config/qtile/config.py".source = ./config.py;
+  };
+
 }
